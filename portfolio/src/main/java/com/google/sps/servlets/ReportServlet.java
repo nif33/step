01642d19d9a37300cfff95f4b485f5703d93b498
Jennifer <jennifer_dai2000@yahoo.ca>
2020-06-10 11:44:47 -0700

    move report increment to a function
    change retry counter loop into a for loop
    move report increment into a function called by the loop
    indentation fixes

--- portfolio/src/main/java/com/google/sps/servlets/ReportServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ReportServlet.java
@@ -22,37 +22,39 @@ public class ReportServlet extends HttpServlet {
 
   DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
+  public int incrementReportCount(String id, int numRetries) {
+    Transaction transaction = datastore.beginTransaction();
+    try {
+      try {
+        Key commentKey = KeyFactory.stringToKey(id);
+        Entity commentEntity = datastore.get(commentKey);
+        long numReports = (long) commentEntity.getProperty("numReports");
+        numReports++;
+        commentEntity.setProperty("numReports", numReports);
+        datastore.put(transaction, commentEntity);
+        transaction.commit();
+      } catch (EntityNotFoundException e) {
+        System.err.println("Key is not an entity in the database");
+      }
+      return 0;
+    } catch (ConcurrentModificationException e) {
+      if(numRetries == 0) {
+        throw e;
+      }
+      return --numRetries;
+    } finally {
+      if(transaction.isActive()) {
+        transaction.rollback();
+      }
+    }
+  }
+
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String id = request.getParameter("id");
     int numRetries = 3;
-      while(true) {
-        Transaction transaction = datastore.beginTransaction();
-        try {
-          String id = request.getParameter("id");
-          Key commentKey;
-          Entity commentEntity;
-          try {
-            commentKey = KeyFactory.stringToKey(id);
-            commentEntity = datastore.get(commentKey);
-        	} catch (EntityNotFoundException e) {
-        		throw new RuntimeException("Key is not an entity in the database");
-        	}
-          long numReports = (long) commentEntity.getProperty("numReports");
-          numReports++;
-          commentEntity.setProperty("numReports", numReports);
-          datastore.put(transaction, commentEntity);
-          transaction.commit();
-          break;
-        } catch (ConcurrentModificationException e) {
-          if(numRetries == 0) {
-            throw e;
-          }
-          numRetries--;
-        } finally {
-          if(transaction.isActive()) {
-            transaction.rollback();
-          }
-        }
+    while(numRetries > 0) {
+      numRetries = incrementReportCount(id, numRetries);
     }
   }
 }
